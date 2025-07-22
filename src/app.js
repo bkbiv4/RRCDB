54e851b6d2a405140c6671bd33c0c8a31cc9ac6e
bkbiv4 <45056787+bkbiv4@users.noreply.github.com>
2025-07-22 07:33:30 -0400

    Add files via upload
    Updates

---
+++ src/app.js
@@ -0,0 +1,14 @@
+// src/App.js
+
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Rush Royale Companion</h1>
+            <p>Welcome to the card database!</p>
+        </div>
+    );
+}
+
+export default App;
